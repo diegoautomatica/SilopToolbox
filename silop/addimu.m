c89f7a02c0bff55d0a8d49835f72aca1a468702a
dalvarez <>
2008-02-20 09:34:29 +0000

    preparativos para la rotacion

--- silop/addimu.m
+++ silop/addimu.m
@@ -4,13 +4,19 @@
 % Se debe incluir la lista completa de IMUs a usar antes de realizar la conexión
 % 
 % Syntax: 
-%   CONFIG=addimu(posicion,numserie,CONFIG);
+%   CONFIG=addimu(CONFIG,posicion,numserie, orientacion);
 %
 %   Parámetros de entrada: 
+%	CONFIG   -> estructura de configuración de la aplicación antes de añadir el sensor
 % 	posicion -> Cadena de texto conteniendo la posición en la que está el sensor.
 %		    Puede ser: 'COG','MUSLO_DCHO','MUSLO_IZDO','TIBIA_DCHA','TIBIA_IZDA','PIE_DCHO' o 'PIE_IZDO'.
 %	numserie -> numero de serie
-%	CONFIG   -> estructura de configuración de la aplicación antes de añadir el sensor
+%       orientacion -> Vector de tres elementos que debe indicar cual es la dirección antero-posterior,
+%                   medio-lateral y vertical referida a los ejes del acelerómetro. X=1,Y=2,Z=3.
+%                   Por defecto vale [3,2,1]=[antero-posterior=Z del acelerómetro, Medio-lateral=Y del acelerómetro,
+%                   vertical=X del acelerómetro].
+%		    Se aceptan valores negativos para indicar que el eje anatómico y el del acelerómetro son opuestos.
+%	            
 %   Parámetros de salida: 
 %	CONFIG  Estructura de configuración de la aplicación después de añadir el sensor
 % 
@@ -24,26 +30,37 @@
 %           24.01.2008 Incorporado a la toolbox
 %
 
-function CONFIG=addimu(donde, serie, conf)
-
-    CONFIG = conf;
+function CONFIG=addimu(CONFIG, donde, serie, R)
+	
+	if (nargin<3)
+		error('es necesario especificar los tres primeros parámetros');
+	elseif (nargin<4)
+		R=[3,2,1];
+	end
 
     switch donde
         
         case 'COG',
             CONFIG.SENHALES.COG.Serie = serie;
+            CONFIG.SENHALES.COG.R = R;
         case 'MUSLO_DCHO',
             CONFIG.SENHALES.MUSLO_DCHO.serie = serie;
+            CONFIG.SENHALES.MUSLO_DCHO.R = R;
         case 'MUSLO_IZDO',
             CONFIG.SENHALES.MUSLO_IZDO.serie = serie;
+            CONFIG.SENHALES.MUSLO_IZDO.R = R;
         case 'TIBIA_DCHA',
             CONFIG.SENHALES.TIBIA_DCHA.serie = serie;
+            CONFIG.SENHALES.TIBIA_DCHA.R = R;
         case 'TIBIA_IZDA',
             CONFIG.SENHALES.TIBIA_IZDA.serie = serie;
+            CONFIG.SENHALES.TIBIA_IZDA.R = R;
         case 'PIE_DCHO',
             CONFIG.SENHALES.PIE_DCHO.serie = serie;
+            CONFIG.SENHALES.PIE_DCHO.R = R;
         case 'PIE_IZDO',
             CONFIG.SENHALES.PIE_IZDO.serie = serie;
+            CONFIG.SENHALES.PIE_IZDO.R = R;
             
         otherwise
             warning('posición de IMU no reconocida')
