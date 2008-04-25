b67b0a2801c2c07d5ca089dfbb4782c925be0e72
dalvarez <>
2008-04-25 10:35:46 +0000

    Usa el nuevo algoritmo alg_plot_pos2d que recuerda toda la señal

--- silop/private/alg_plot_pos2d.m
+++ silop/private/alg_plot_pos2d.m
@@ -9,22 +9,54 @@
 
 %Creado: 06-02-2008 por Diego
 
-function alg_plot_pos2d(valores_actuales, senhales, params, dependencias) %#ok<INUSL>
-%    C = AddAlgoritmo(C, 'alg_plot_pos2d', 0, [], [], {'alg_est_2d'}); 
+function yadibujado=alg_plot_pos2d(resultados, senhales, params, dependencias) %#ok<INUSL>
+
 
 
 	persistent mifigura
-	if (isempty(mifigura))
+    persistent midata
+    if (isempty(mifigura))
     		mifigura=figure;
-	end
+            midata=[0,0];
+    end
+    try
+        basura=get(mifigura);   %#ok<NASGU> %Intentamos pillar la figura
+    catch                % si no existe
+        midata=[0,0];    % los datos antiguos no tienen sentido
+    end
+ 
+    figure(mifigura);
+    ultimodibujado=find(~isnan(resultados),1,'last');
+    if (isempty(ultimodibujado))
+        ultimodibujado=1;
+    end
+    indices=find(~isnan(dependencias(ultimodibujado+1:end,1)))+ultimodibujado;
+    yadibujado=resultados;
+    if (~isempty(indices))
+        yadibujado(1:max(indices))=1;
+        midata=[midata;dependencias(indices,1), dependencias(indices,2)];
+        plot(midata(:,1),midata(:,2));
+        drawnow;
+    end
 
 
-    figure(mifigura);    
-    
-    indices=find(~isnan(dependencias(:,1)));
-    plot(dependencias(indices,1), dependencias(indices,2));
-    drawnow;
 
-    %subplot(212);
-    %bar(dependencias)
+
+
+
+
+
+% Old working code
+%function alg_plot_pos2d(valores_actuales, senhales, params, dependencias) %#ok<INUSL>
+%     persistent mifigura
+% 	if (isempty(mifigura))
+%     		mifigura=figure;
+% 	end
+% 
+% 
+%     figure(mifigura);    
+%     
+%     indices=find(~isnan(dependencias(:,1)));
+%     plot(dependencias(indices,1), dependencias(indices,2));
+%     drawnow;
 
