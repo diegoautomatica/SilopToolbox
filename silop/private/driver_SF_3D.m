6f6d11b0c3fc26824029e6aa89cd3751395b02d9
dalvarez <>
2008-07-29 20:34:25 +0000

    driver_SF_3D absorbe sf3dsetperiod

--- silop/private/driver_SF_3D.m
+++ silop/private/driver_SF_3D.m
@@ -207,3 +207,68 @@ function leersf3dData(obj,event,sf3d)  %#ok<INUSL>
     end
     disp('leido un segundo de datos');
 end
+
+
+function sf3d=sf3dsetperiod(sf3d)
+
+    freq=sf3d.freq;
+    if (sf3d.puerto.BytesAvailable>0)
+        disp(['>>> AVISO: Se descartaran ' int2str(sf3d.puerto.BytesAvailable) ' datos']);
+        fread(sf3d.puerto, sf3d.puerto.BytesAvailable,'uint8');
+    end
+    % Cuerpo del mensaje 
+    msg='3';
+    fwrite(sf3d.puerto,msg,'uint8','async');
+    % Se espera a recibir la contestacion
+    [ack,cnt,msg]=fread(sf3d.puerto,78,'uint8');
+    if (~isempty(msg))
+        disp(msg);
+        error('no se ha recibido respuesta al comando setperiod');
+    end
+    if (sf3d.puerto.BytesAvailable>0)
+        fread(sf3d.puerto, sf3d.puerto.BytesAvailable,'uint8');
+    end
+    ack=ack(end-10:end-1);%Me quedo con los 10 penultimos, que se parecen al resto de respuestas
+    if ack(10)=='H'
+        %solo dos cifras
+        frecuencia=str2double(char(ack(8:9)));
+    else
+        %tres cifras
+        frecuencia=str2double(char(ack(8:10)));
+    end
+    while (frecuencia~=freq)
+        if (frecuencia>freq)
+            msg='-';
+        else
+            msg='+';
+        end
+        fwrite(sf3d.puerto,msg,'uint8','async');
+        % Se espera a recibir la contestacion
+        [ack,cnt,msg]=fread(sf3d.puerto,10,'uint8');
+        if (~isempty(msg))
+            disp(msg);
+            error('no se ha recibido respuesta al cambiar la frecuencia de muestreo');
+        end
+        if (sf3d.puerto.BytesAvailable>0)
+            fread(sf3d.puerto, sf3d.puerto.BytesAvailable,'uint8');
+        end
+        if ack(8)=='H'
+            %solo dos cifras
+            frecuencia=str2double(char(ack(6:7)));
+        else
+            %tres cifras
+            frecuencia=str2double(char(ack(6:8)));
+        end
+    end
+    %Salimos del modo de frecuencia
+    msg='x';
+    fwrite(sf3d.puerto,msg,'uint8','async');
+    [ack,cnt,msg]=fread(sf3d.puerto, 196,'uint8');
+    if (~isempty(msg))
+        disp(msg);
+        error('no se ha recibido respuesta al salir del ajuste de frecuencia');
+    end
+    if (sf3d.puerto.BytesAvailable>0)
+        fread(sf3d.puerto, sf3d.puerto.BytesAvailable,'uint8');
+    end
+end
