1109de66d911d0b25ab435cb838d1ac078f6241f
dalvarez <>
2011-02-03 15:58:19 +0000

    bug en la variable persistente

--- silop/private/alg_plot_senhales.m
+++ silop/private/alg_plot_senhales.m
@@ -4,16 +4,18 @@
 %Se puede configurar mediante:
 %    addalgoritmo('alg_plot_senhales', 0, {lista de señales}, []);
 %
-%Parámetros: como todos los alg_*. resultados anteriores, señales a usar y parametros(vacio en este caso).
+%Parámetros: como todos los alg_*. resultados anteriores, señales a usar y 
+%parametros. Si los parametros están vacios se crea una nueva ventana, 
+% en caso contrario se dibuja sobre los axes indicados.
+% Ej: addalgoritmo('alg_plot_senhales',0,{'COG.Acc_X'},handles.axes1);
 
 %Creado: 01-02-2008 por Diego
 
 function alg_plot_senhales(valores_actuales, senhales, params) %#ok<INUSD,INUSL>
 %    C = AddAlgoritmo(C, 'alg_plot_senhales', 0, [COG.Acc_X COG.Acc_Y COG.Acc_Z], [], {'det_mov_pot_acc_v_cog'}); 
 
-
+    persistent mifigura
     if (isempty(params))
-        persistent mifigura
     %axes(params)
         if (isempty(mifigura))
     		mifigura=figure;
